fcdf145964d260889518021bd153c291c2f67c0e
aliaksandrZh <108681764+aliaksandrZh@users.noreply.github.com>
2026-01-04 20:32:44 +0300

    base state machine

--- GitAssistant.Core/StateMachines/BaseStateMachine.cs
+++ GitAssistant.Core/StateMachines/BaseStateMachine.cs
@@ -1,12 +1,58 @@
 using Stateless;
 
-public abstract class BaseStateMachine<TState, TTrigger>
+public interface ISubMachine
+{
+    public event Action OnCompleted;
+    public object CurrentState { get; }
+    public ISubMachine? CurrentSubMachine { get; set; }
+    public object? CurrentStateSubState { get; }
+
+    void Fire(object trigger);
+
+    public void RiseCompleted();
+    // void Activate();
+    // void Deactivate();
+
+}
+
+public abstract class BaseStateMachine<TState, TTrigger> : ISubMachine
     where TState : struct, Enum
     where TTrigger : struct, Enum
 {
-    protected readonly StateMachine<TState, TTrigger> _sm;
+    public event Action? OnCompleted;
 
-    public TState CurrentState => _sm.State;
+    protected StateMachine<TState, TTrigger> _sm;
+
+    public object CurrentState => _sm.State;
+    public ISubMachine? CurrentSubMachine { get; set; }
+    public object? CurrentStateSubState => CurrentSubMachine?.CurrentState;
+
+    // public void Activate() => _sm.Activate();
+
+    // public void Deactivate()
+    // {
+    //     if (CurrentSubMachine != null)
+    //     {
+    //         CurrentSubMachine.Deactivate();
+    //         CurrentSubMachine = null;
+    //     }
+
+    //     _sm.Deactivate();
+    // }
+
+    public void RiseCompleted()
+    {
+        if (CurrentSubMachine != null)
+        {
+            Console.WriteLine($"[{GetType().Name}] has an active sub machine");
+            // CurrentSubMachine.RiseCompleted();
+            // CurrentSubMachine = null;
+        }
+
+        // _sm.Deactivate();
+        OnCompleted?.Invoke();
+        Console.WriteLine($"[{GetType().Name}] Stopped at state: {CurrentState}");
+    }
 
     protected BaseStateMachine(TState initialState)
     {
@@ -14,7 +60,30 @@ public abstract class BaseStateMachine<TState, TTrigger>
         PerformConfiguration();
     }
 
-    public void Fire(TTrigger trigger) => _sm.Fire(trigger);
+    protected void FireInternal(TTrigger trigger) => _sm.Fire(trigger);
+
+    public void Fire(TTrigger trigger)
+    {
+        if (CurrentSubMachine != null)
+        {
+            Console.WriteLine($"[{GetType().Name}] Busy: Ignoring trigger {trigger}");
+            return;
+        }
+        _sm.Fire(trigger);
+    }
+    public void Fire(object dynamicTrigger)
+    {
+        if (CurrentSubMachine is ISubMachine sub)
+        {
+            sub.Fire(dynamicTrigger);
+            return;
+        }
+
+        if (dynamicTrigger is TTrigger parentTrigger)
+        {
+            Fire(parentTrigger);
+        }
+    }
 
     public bool IsInState(TState state) => _sm.IsInState(state);
 
@@ -24,24 +93,32 @@ public abstract class BaseStateMachine<TState, TTrigger>
     protected StateMachine<TState, TTrigger>.StateConfiguration Configure(TState state)
         => _sm.Configure(state);
 
-    // protected StateMachine<TState, TTrigger>.StateConfiguration /* void */ ConfigureSubMachine<TSState, TTSTrigger>(
-    //     TState state,
-    //     TTrigger completeTrigger,
-    //     BaseStateMachine<TSState, TTSTrigger> subStateMachine,
-    //     TTrigger abortTrigger = default)
-    //     where TSState : struct, Enum
-    //     where TTSTrigger : struct, Enum
-    // {
-    //     // _subMachineRegistry.Register(state, subStateMachine);
-    //     subStateMachine.OnCompleted += () => _sm.Fire(completeTrigger);
-    //     // subStateMachine._sm.OnTransitionCompleted(t =>
-    //     // {
+    protected StateMachine<TState, TTrigger>.StateConfiguration ConfigureSubMachine<TSState, TTSTrigger>(
+        TState state,
+        BaseStateMachine<TSState, TTSTrigger> subStateMachine,
+        Action onSubMachineCompleted
+        )
+        where TSState : struct, Enum
+        where TTSTrigger : struct, Enum
+    {
+        return _sm.Configure(state)
+            .OnEntry(() =>
+            {
+                CurrentSubMachine = subStateMachine;
+                subStateMachine.OnCompleted += onSubMachineCompleted;
+            })
+            .OnExit(() =>
+            {
+                if (CurrentSubMachine is null)
+                {
+                    Console.WriteLine("CurrentSubMachine is already null!");
+                    return;
+                }
 
-    //     // });
-    //     return _sm.Configure(state);
-    //     // .OnEntry(() => _subMachineRegistry.Activate(state))
-    //     // .OnExit(() => _subMachineRegistry.Deactivate());
-    // }
+                subStateMachine.OnCompleted -= onSubMachineCompleted;
+                CurrentSubMachine = null;
+            });
+    }
 
     private void PerformConfiguration()
     {
---
+++ GitAssistant.Tests/Core/StateMachines/BaseStateMachineTests.cs
@@ -0,0 +1,149 @@
+public class StateMachineUnitTests
+{
+    public enum PState { Idle, Running, Finished }
+    public enum PTrigger { Start, SubDone }
+
+    public enum CState { Init, Working, Done }
+    public enum CTrigger { Go, Finish }
+
+    public enum GCState { GStart, GDone }
+    public enum GCTrigger { GFinish }
+
+    public class GrandParentMachine : BaseStateMachine<PState, PTrigger>
+    {
+        public ParentMachine Parent { get; } = new ParentMachine();
+        public GrandParentMachine() : base(PState.Idle) { }
+
+        protected override void ConfigureMachine()
+        {
+            Configure(PState.Idle).Permit(PTrigger.Start, PState.Running);
+            Configure(PState.Running).Permit(PTrigger.SubDone, PState.Finished);
+            ConfigureSubMachine(PState.Running, Parent, () => FireInternal(PTrigger.SubDone));
+            Configure(PState.Finished).Ignore(PTrigger.SubDone);
+        }
+    }
+
+    public class ParentMachine : BaseStateMachine<PState, PTrigger>
+    {
+        public ChildMachine Child { get; } = new ChildMachine();
+        public ParentMachine() : base(PState.Idle) { }
+
+        protected override void ConfigureMachine()
+        {
+            Configure(PState.Idle).Permit(PTrigger.Start, PState.Running);
+            Configure(PState.Running).Permit(PTrigger.SubDone, PState.Finished);
+            ConfigureSubMachine(PState.Running, Child, () => FireInternal(PTrigger.SubDone));
+            Configure(PState.Finished).Ignore(PTrigger.SubDone);
+        }
+    }
+
+    public class ChildMachine : BaseStateMachine<CState, CTrigger>
+    {
+        public ChildMachine() : base(CState.Init) { }
+        protected override void ConfigureMachine()
+        {
+            Configure(CState.Init).Permit(CTrigger.Go, CState.Working);
+            Configure(CState.Working).Permit(CTrigger.Finish, CState.Done);
+            Configure(CState.Done).OnEntry(() => RiseCompleted());
+        }
+    }
+
+    public class GrandChildMachine : BaseStateMachine<GCState, GCTrigger>
+    {
+        public GrandChildMachine() : base(GCState.GStart) { }
+        protected override void ConfigureMachine()
+        {
+            Configure(GCState.GStart).Permit(GCTrigger.GFinish, GCState.GDone);
+            Configure(GCState.GDone).OnEntry(() => RiseCompleted());
+        }
+    }
+
+
+    [Fact]
+    public void Parent_ShouldBlockInput_WhenSubMachineIsActive()
+    {
+        var parent = new ParentMachine();
+        // parent.Activate();
+        parent.Fire(PTrigger.Start);
+
+        parent.Fire(PTrigger.SubDone); // Should be ignored
+
+        Assert.Equal(PState.Running, (PState)parent.CurrentState);
+        Assert.NotNull(parent.CurrentSubMachine);
+    }
+
+    [Fact]
+    public void Parent_ShouldExpose_ChildState()
+    {
+        var parent = new ParentMachine();
+        // parent.Activate();
+        parent.Fire(PTrigger.Start);
+
+        parent.Fire((object)CTrigger.Go);
+
+        Assert.Equal(CState.Working, parent.CurrentStateSubState);
+    }
+
+    [Fact]
+    public void SubMachineCompletion_ShouldTrigger_ParentTransition()
+    {
+        var parent = new ParentMachine();
+        // parent.Activate();
+        parent.Fire(PTrigger.Start);
+
+        parent.Fire((object)CTrigger.Go);
+        parent.Fire((object)CTrigger.Finish);
+
+        Assert.Equal(PState.Finished, (PState)parent.CurrentState);
+        Assert.Null(parent.CurrentSubMachine);
+    }
+
+    [Fact]
+    public void DeepNesting_ShouldPropagateTriggers_AndCleanup_Automatically()
+    {
+        var grandParent = new GrandParentMachine();
+        // grandParent.Activate();
+
+        grandParent.Fire(PTrigger.Start);
+        grandParent.Fire((object)PTrigger.Start); // Starts the internal ParentMachine
+
+        // Grandparent(Running) -> Parent(Running) -> Child(Init)
+        Assert.Equal(CState.Init, grandParent.CurrentSubMachine?.CurrentStateSubState);
+
+        grandParent.Fire((object)CTrigger.Go);
+        grandParent.Fire((object)CTrigger.Finish);
+
+        // Cascade completion should reach the top
+        Assert.Equal(PState.Finished, (PState)grandParent.CurrentState);
+        Assert.Null(grandParent.CurrentSubMachine);
+    }
+
+    [Fact]
+    public void ReEntry_ShouldRehookSubMachineEvents()
+    {
+        var parent = new LoopingParentMachine();
+        // parent.Activate();
+
+        // Pass 1
+        parent.Fire(PTrigger.Start);
+        parent.Fire((object)CTrigger.Go);
+        parent.Fire((object)CTrigger.Finish);
+        Assert.Equal(PState.Finished, (PState)parent.CurrentState);
+
+        // Pass 2
+        parent.Fire(PTrigger.Start);
+        parent.Fire((object)CTrigger.Go);
+        parent.Fire((object)CTrigger.Finish);
+
+        Assert.Equal(PState.Finished, (PState)parent.CurrentState);
+    }
+
+    public class LoopingParentMachine : ParentMachine
+    {
+        protected override void ConfigureMachine()
+        {
+            base.ConfigureMachine();
+            Configure(PState.Finished).Permit(PTrigger.Start, PState.Running);
+        }
+    }
+}
